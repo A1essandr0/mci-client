e0ae531173ac8284b950ab289cecdf810518ff80
Alexander Kochurov <the.solarstone@gmail.com>
2021-10-04 22:28:46 +0300

    func tests WIP

--- functests/src/testConfig.ts
+++ functests/src/testConfig.ts
@@ -1,6 +1,15 @@
+import { server_url } from  '../../src/code/urls';
+
+let [_, host, port] = server_url.split(':');
 
 const tConfig = {
     fixturesDir: "./functests/fixtures",
+    server_host: host.slice(2),
+    server_port: port,
+
+    testUserName: 'Tester',
+    testUserEmail: 'tester@tester.com',
+    testUserPassword: '2wsxzse4',
 
 }
 
