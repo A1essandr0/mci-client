be29cd769e5b95e5d08b1c518a19afda4f7673b4
Alexander Kochurov <akrendering@mail.ru>
2022-11-05 05:06:54 +0300

    api.memoricci.fun -> memoricci.fun

--- src/code/config.ts
+++ src/code/config.ts
@@ -3,7 +3,7 @@ export const config = {
     mode: process.env.CLIENT_MODE,
 
     local_server_url: 'https://localhost:3003',
-    azurevm_server_url: 'https://api.memoricci.fun:3003',
+    azurevm_server_url: 'https://memoricci.fun:3003',
     application_url: 'https://www.memoricci.fun',
 
     gameStartingDelay: 7,
